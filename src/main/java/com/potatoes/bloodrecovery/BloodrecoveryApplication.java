889b16f57ce1f343887a6661e8e7b91e65bfe10f
hyunyoung <chy9386@gmail.com>
2023-06-11 14:06:33 +0900

    Initial commit

---
+++ src/main/java/com/potatoes/bloodrecovery/BloodrecoveryApplication.java
@@ -0,0 +1,13 @@
+package com.potatoes.bloodrecovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodrecoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodrecoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/potatoes/bloodrecovery/BloodrecoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potatoes.bloodrecovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodrecoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
