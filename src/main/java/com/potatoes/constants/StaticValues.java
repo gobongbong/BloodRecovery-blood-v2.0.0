fd3d18635263276fb80f6165acc395e6150537e6
hyunyoung <chy9386@gmail.com>
2023-08-06 18:38:19 +0900

    [NEW] Feign Client 초기 설정, CustomerFeignClient 추가

---
+++ src/main/java/com/potatoes/constants/StaticValues.java
@@ -0,0 +1,7 @@
+package com.potatoes.constants;
+
+public class StaticValues {
+
+    public static final String RESULT_CODE = "resultCode";
+    public static final String RESULT_MESSAGE = "resultMessage";
+}
