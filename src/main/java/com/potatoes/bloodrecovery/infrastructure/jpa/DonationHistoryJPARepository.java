0c01bb82f1c445d067a4c2f8e2bb299959d687d1
hyunyoung <chy9386@gmail.com>
2024-03-03 15:45:04 +0900

    [NEW] 기부 이력 조회 API

--- src/main/java/com/potatoes/bloodrecovery/infrastructure/jpa/DonationHistoryJPARepository.java
+++ src/main/java/com/potatoes/bloodrecovery/infrastructure/jpa/DonationHistoryJPARepository.java
@@ -4,5 +4,8 @@ import com.potatoes.bloodrecovery.domain.model.aggregates.DonationHistory;
 import com.potatoes.bloodrecovery.domain.repository.DonationHistoryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DonationHistoryJPARepository extends JpaRepository<DonationHistory, Long>, DonationHistoryRepository {
+    List<DonationHistory> findByCid(String cid);
 }
