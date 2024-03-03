e4f1f962c5edb7b7231ed293c11005cf10db1cbf
hyunyoung <chy9386@gmail.com>
2024-03-03 15:45:04 +0900

    [NEW] 기부 이력 조회 API

--- src/main/java/com/potatoes/bloodrecovery/domain/repository/DonationHistoryRepository.java
+++ src/main/java/com/potatoes/bloodrecovery/domain/repository/DonationHistoryRepository.java
@@ -2,6 +2,9 @@ package com.potatoes.bloodrecovery.domain.repository;
 
 import com.potatoes.bloodrecovery.domain.model.aggregates.DonationHistory;
 
+import java.util.List;
+
 public interface DonationHistoryRepository {
     DonationHistory save(DonationHistory donationHistory);
+    List<DonationHistory> findByCid(String cid);
 }
--- src/main/java/com/potatoes/bloodrecovery/interfaces/rest/controller/BloodDonationController.java
+++ src/main/java/com/potatoes/bloodrecovery/interfaces/rest/controller/BloodDonationController.java
@@ -1,8 +1,10 @@
 package com.potatoes.bloodrecovery.interfaces.rest.controller;
 
 import com.potatoes.bloodrecovery.application.commandservices.DonationBloodCardCommandService;
+import com.potatoes.bloodrecovery.application.queryservices.GetDonationHistoryQueryService;
 import com.potatoes.bloodrecovery.domain.model.commands.DonationBloodCardCommand;
 import com.potatoes.bloodrecovery.interfaces.rest.dto.DonationBloodCardReqDto;
+import com.potatoes.bloodrecovery.interfaces.rest.dto.GetDonationHistoryRspDto;
 import com.potatoes.bloodrecovery.interfaces.rest.mapper.DonationBloodMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,11 +28,20 @@ public class BloodDonationController extends BaseController{
 
     private final DonationBloodCardCommandService donationBloodCardCommandService;
     private final DonationBloodMapper donationBloodMapper;
+    private final GetDonationHistoryQueryService getDonationHistoryQueryService;
 
     @PostMapping(DONATION_BLOOD_CARD)
     public ResponseEntity<Object> donationBloodCard(@RequestHeader(value = HEADER_CID) String cid, @RequestBody @Valid DonationBloodCardReqDto donationBloodCardReqDto) {
         DonationBloodCardCommand donationBloodCardCommand = donationBloodMapper.donationBloodCardReqtoCommand(cid, donationBloodCardReqDto);
         donationBloodCardCommandService.donationBloodCard(donationBloodCardCommand);
         return new ResponseEntity<>(getSuccessHeaders(), HttpStatus.OK);
+    }
+
+    @GetMapping(DONATION_BLOOD_CARD)
+    public ResponseEntity<Object> getDonationHistory(@RequestHeader(value = HEADER_CID) String cid){
+        GetDonationHistoryRspDto getDonationHistoryRspDto = GetDonationHistoryRspDto.builder()
+                .donationHistory(getDonationHistoryQueryService.getDonationHistory(cid))
+                .build();
+        return new ResponseEntity<>(getDonationHistoryRspDto, getSuccessHeaders(), HttpStatus.OK);
     }
 }
