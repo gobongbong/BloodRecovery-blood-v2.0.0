f29dfb255691eeb9b192ea882b72f5d5e41588b8
hyunyoung <chy9386@gmail.com>
2024-06-08 17:15:47 +0900

    [FIX] 헌혈증 기부 API 로직 수정 + 알람 호출 부분 todo 추가 [DELETE] 헌혈증 OCR API 삭제 [IMPROVED] BloodCardController validation 추가

--- src/main/java/com/potatoes/bloodrecovery/interfaces/rest/mapper/BloodCardMapper.java
+++ src/main/java/com/potatoes/bloodrecovery/interfaces/rest/mapper/BloodCardMapper.java
@@ -2,9 +2,6 @@ package com.potatoes.bloodrecovery.interfaces.rest.mapper;
 
 import com.potatoes.bloodrecovery.domain.model.commands.DeleteBloodCardCommand;
 import com.potatoes.bloodrecovery.domain.model.commands.RegisterBloodCardCommand;
-import com.potatoes.bloodrecovery.domain.model.queries.GetBloodCardsQuery;
-import com.potatoes.bloodrecovery.interfaces.rest.dto.DeleteBloodCardReqDto;
-import com.potatoes.bloodrecovery.interfaces.rest.dto.GetBloodCardsReqDto;
 import com.potatoes.bloodrecovery.interfaces.rest.dto.RegisterBloodCardReqDto;
 import com.potatoes.config.MapstructConfig;
 import org.mapstruct.Mapper;
