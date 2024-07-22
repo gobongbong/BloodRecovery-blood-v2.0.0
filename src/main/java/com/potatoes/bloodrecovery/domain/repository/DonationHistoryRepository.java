47b24a09f5046a27ab4fac8e695489cc8ef7ea34
kim-na-ram <snrneh3@naver.com>
2024-07-22 14:26:52 +0900

    [FIX] 지정 헌혈 요청 API 수정
    - 지정 헌혈 요청할 때 지정 헌혈 이력 존재 시, 500 대신 400을 return하도록 변경
    - 사용하지 않는 Validation Custom Class 제거
    - 변수명을 직관적으로 수정 등

--- src/main/java/com/potatoes/bloodrecovery/domain/repository/DonationHistoryRepository.java
+++ src/main/java/com/potatoes/bloodrecovery/domain/repository/DonationHistoryRepository.java
@@ -10,5 +10,5 @@ public interface DonationHistoryRepository {
     List<DonationHistory> findByCid(String cid);
     List<DonationHistory> findByCidAndDonationTypeAndDonationStatus(String cid, String donationType, DonationStatus requestStatus);
     List<DonationHistory> findByRequestIdAndDonationType(Long requestId, String donationType);
-    boolean existsByCidAndDonationTypeAndDonationStatus(String cid, String donationType, DonationStatus requestStatus);
+    boolean existsByCidAndDonationTypeAndDonationStatus(String cid, String donationType, String requestStatus);
 }
--- src/main/java/com/potatoes/bloodrecovery/interfaces/rest/dto/DirectedBloodDonationReqDto.java
+++ src/main/java/com/potatoes/bloodrecovery/interfaces/rest/dto/DirectedBloodDonationReqDto.java
@@ -1,6 +1,5 @@
 package com.potatoes.bloodrecovery.interfaces.rest.dto;
 
-import com.potatoes.bloodrecovery.interfaces.rest.validation.DirectedBloodDonationReqValidation;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,9 +11,7 @@ import javax.validation.constraints.NotNull;
 @Builder
 @ToString
 @AllArgsConstructor
-@DirectedBloodDonationReqValidation
 public class DirectedBloodDonationReqDto {
-    // todo validation 추가 필요
-    @NotNull(message = "")
+    @NotNull
     private Long requestId;
 }
