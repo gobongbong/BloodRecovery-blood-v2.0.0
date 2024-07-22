cf8ace44393c7933e70ab104941b8b79bbda720e
HyunYoung <chy9386@gmail.com>
2024-07-22 15:15:38 +0900

    [FIX] DonationHistoryJPARepository 수정

--- src/main/java/com/potatoes/bloodrecovery/infrastructure/jpa/DonationHistoryJPARepository.java
+++ src/main/java/com/potatoes/bloodrecovery/infrastructure/jpa/DonationHistoryJPARepository.java
@@ -2,6 +2,7 @@ package com.potatoes.bloodrecovery.infrastructure.jpa;
 
 import com.potatoes.bloodrecovery.domain.model.aggregates.DonationHistory;
 import com.potatoes.bloodrecovery.domain.repository.DonationHistoryRepository;
+import com.potatoes.constants.DonationStatus;
 import com.potatoes.constants.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,6 +10,6 @@ import java.util.List;
 
 public interface DonationHistoryJPARepository extends JpaRepository<DonationHistory, Long>, DonationHistoryRepository {
     List<DonationHistory> findByCid(String cid);
-    List<DonationHistory> findByCidAndDonationTypeAndDonationStatus(String cid, String donationType, RequestStatus requestStatus);
+    List<DonationHistory> findByCidAndDonationTypeAndDonationStatus(String cid, String donationType, DonationStatus requestStatus);
     List<DonationHistory> findByRequestIdAndDonationType(Long requestId, String donationType);
 }
